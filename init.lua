8a75a70ada75523438a95b2f151673cee65cbc1e
jardimdanificado <jardeltorreslima01@gmail.com>
2023-08-26 04:15:50 -0300

    experimental build using raylib

--- init.lua
+++ init.lua
@@ -1,39 +1,40 @@
 --Mouse Centered Graphical User Interface(MoCeGUI)
-local mocegui={version="0.1.6"}
-local requirePath = love.filesystem.getRequirePath()
-love.filesystem.setRequirePath('mocegui/luatils/?.lua;' .. requirePath)
 package.path = 'mocegui/luatils/?.lua' .. ";" .. package.path
-local util = require "mocegui.luatils"
+local mocegui={version="0.1.7",pending = {},font={}}
+local util = require "mocegui.luatils.init"
 mocegui.util = util
+local options = require "data.config"
 local mouse =
 {
-	draggin = false
+	draggin = false,
+	lastposition = {x=0,y=0},
+	moved = {x=0,y=0},
 }
 
 mocegui.window = {}
 
 function mocegui.closeWindow()
 	mocegui.window[1] = nil
-	util.array.selfclear(mocegui.window)
+	mocegui.window = util.array.clear(mocegui.window)
 end
 
 function mocegui.newWindow(title,position,size,color)
 	local win = 
 	{
 		func = nil,
-		color= color or {0.3,0.3,0.4,1},
-		position= position or {16,16},
-		size=size or {16,16},
+		color= color or {76,76,104,255},
+		position= position or {x=16,y=16},
+		size=size or {x=16,y=16},
 		text = {},
 		button = not title and 
 		{
 		} or
 		{
 			{
-				position = {(size and size[1] or 50)-12,0},
-				size = {12,12},
-				color = {1,0.5,0.5,1},
-				pcolor = {0.5,0.1,0.1,1},
+				position = {x=(size and size.x or 50)-12,y=0},
+				size = {x=12,y=12},
+				color = {r=255,g=127,b=127,a=255},
+				pcolor = {r=127,g=25,b=25,a=255},
 				func = mocegui.closeWindow,
 				args = {}
 			}
@@ -42,25 +43,40 @@ function mocegui.newWindow(title,position,size,color)
 		title = title
 	}
 	win.text.new = function(text,position,size,color,pcolor)
-		local txt = {
-			position = position or {0,0},
-			size = size or {1,1},
-			color = color or {1,1,1,1},
-			text = text or 'blank'
-		}
-		table.insert(win.text,1,txt)
-		return win.text[1]
+		local newtxt = function (txt)
+			return {
+				position = {x=position.x or 0,y= position.y or 0},
+				size = size or 12,
+				color = {r=255 or color.r,g=255 or color.g,b=255 or color.b,a=255 or color.a},
+				text = (txt .. '') or 'blank'
+			}
+		end
+		if util.string.includes(text,'\n') then
+			local result = {}
+			local temp
+			for key, value in ipairs(util.string.split(text,'\n')) do
+				print(value)
+				temp = newtxt(value)
+				temp.position.y = temp.position.y + (12*(key-1))
+				table.insert(win.text,1,temp)
+				table.insert(result,win.text[1])
+			end
+			return result
+		else
+			table.insert(win.text,1,newtxt(text))
+			return win.text[1]
+		end
 	end
 	win.button.new = function(position,size,func,args,color,pcolor)
 		local btn = {
 			position = position,
-			size = size or {12,12},
-			color = color or {0,0.5,0.5,1},
-			pcolor = pcolor or {0.5,0.1,0.1,1},
+			size = size or {x=12,y=12},
+			color = color or {r=0,g=127,b=127,a=255},
+			pcolor = pcolor or {r=127,g=25,b=26,a=255},
 			func = func or mocegui.closeWindow,
 			args = args or {}
 		}
-		btn.position = position or {size[1]-12,0}
+		btn.position = position or {x=size.x-12,y=0}
 		table.insert(win.button,1,btn)
 		return win.button[1]
 	end
@@ -70,48 +86,61 @@ function mocegui.newWindow(title,position,size,color)
 end
 
 local function bRect(px,py,sx,sy,color,bordercolor)
-	love.graphics.setColor(bordercolor or {1,1,1,1})
-	love.graphics.rectangle( 'fill', px-1, py-1, sx+2, sy+2)
-	love.graphics.setColor(unpack(color))
-	love.graphics.rectangle( 'fill', px, py, sx, sy)
+	rl.DrawRectangle(px-1, py-1, sx+2, sy+2, bordercolor or rl.WHITE)
+	rl.DrawRectangle(px, py, sx, sy, color or rl.BLUE)
 end
 
 function mocegui.load()
-	local iconImageData = love.image.newImageData("mocegui/png/icon.png")
-    love.window.setIcon(iconImageData)
-	love.window.setTitle("MoCeGUI-" .. mocegui.version)
-	mocegui.titlecache = love.window.getTitle()
+	if options.fullscreen then
+        rl.SetConfigFlags(rl.FLAG_FULLSCREEN_MODE)
+    end
+    if options.vsync then
+        rl.SetConfigFlags(rl.FLAG_VSYNC_HINT)
+    end
+    if options.runonbackground then
+        rl.SetConfigFlags(rl.FLAG_WINDOW_ALWAYS_RUN)
+    end
+    if options.msaa then
+        rl.SetConfigFlags(rl.FLAG_MSAA_4X_HINT)
+    end
+    if options.interlace then
+        rl.SetConfigFlags(rl.FLAG_INTERLACED_HINT)
+    end 
+    if options.highdpi then
+        rl.SetConfigFlags(rl.FLAG_WINDOW_HIGHDPI)
+    end
+	rl.SetConfigFlags(rl.FLAG_WINDOW_RESIZABLE)
+    rl.InitWindow(600,400, "MoCeGUI-" .. mocegui.version)
+    rl.SetTargetFPS(0)
+	local iconImageData = rl.LoadImage("mocegui/png/icon.png")
+    rl.SetWindowIcon(iconImageData)
+	mocegui.titlecache = "MoCeGUI-" .. mocegui.version
 
-	local debugwin = mocegui.newWindow('debug window',{1,1},{110,56})
-	debugwin.text.new(mocegui.titlecache, {1,14},{0.9,0.9})
-	local debugtxt = debugwin.text.new("\nCurrent FPS: " .. tostring(love.timer.getFPS()) .. "\nWindow amount:" .. #mocegui.window, {1,14},{0.9,0.9})
+	local debugwin = mocegui.newWindow('debug window',{x=1,y=1},{x=110,y=56})
+	local debugtxt = debugwin.text.new(mocegui.titlecache .. "\nCurrent FPS: " .. tostring(rl.GetFPS()) .. "\nWindow amount:" .. 
+	#mocegui.window, {x=1,y=14},{x=0.9,y=0.9})
+	print(util.stringify(debugtxt))
 	debugwin.func = function ()
-		debugtxt.text = "\nCurrent FPS: " .. tostring(love.timer.getFPS()) .. "\nWindow amount:" .. #mocegui.window
+		debugtxt[3].text = "Window amount:" .. #mocegui.window
+		debugtxt[2].text = "Current FPS: " .. rl.GetFPS()
 	end
-
-	print(mocegui.titlecache)
+	options.rendertexture = rl.LoadRenderTexture(options.screen.x, options.screen.y)
 end
 
 function mocegui.keypressed(key)
 end
 
-function mocegui.mousemoved( x, y, dx, dy, istouch )
-	if mouse.draggin then
-		mouse.draggin[1] = mouse.draggin[1] + dx
-		mouse.draggin[2] = mouse.draggin[2] + dy
-	end
-end
-
-function mocegui.mousereleased(x, y, button)
+function mocegui.mousereleased()
+	local x,y = rl.GetMouseX(),rl.GetMouseY()
 	if mocegui.window[1] then
-		if button == 1 then
+		if rl.IsMouseButtonPressed(0) then
 			mocegui.window[1].button = util.array.clear(mocegui.window[1].button)
 			for index, _button in ipairs(mocegui.window[1].button) do
 				if _button and _button.position and mocegui.window[1] and mocegui.window[1].position then
-					if x >= mocegui.window[1].position[1] + _button.position[1]  and
-					   x <= mocegui.window[1].position[1] + _button.position[1] + _button.size[1]  and
-					   y >= mocegui.window[1].position[2] + _button.position[2]  and
-					   y <= mocegui.window[1].position[2] + _button.position[2] + _button.size[2]  then
+					if x >= mocegui.window[1].position.x + _button.position.x  and
+					   x <= mocegui.window[1].position.x + _button.position.x + _button.size.x  and
+					   y >= mocegui.window[1].position.y + _button.position.y  and
+					   y <= mocegui.window[1].position.y + _button.position.y + _button.size.y  then
 						_button.func(mocegui.window)
 						_button.pressed = false
 					end
@@ -120,103 +149,151 @@ function mocegui.mousereleased(x, y, button)
 			if mouse.draggin then
 				mouse.draggin = false
 			end
-		elseif button == 3 then
+		elseif rl.IsMouseButtonPressed(2) then
 			if mouse.draggin then
 				mouse.draggin = false
 			end
-		elseif button == 2 then
-			if x >= mocegui.window[1].position[1]  and
-			x <= mocegui.window[1].position[1] + mocegui.window[1].size[1]  and
-			y >= mocegui.window[1].position[2]  and
-			y <= mocegui.window[1].position[2] + mocegui.window[1].size[2]  then
+		elseif rl.IsMouseButtonPressed(1) then
+			if x >= mocegui.window[1].position.x  and
+			x <= mocegui.window[1].position.x + mocegui.window[1].size.x  and
+			y >= mocegui.window[1].position.y  and
+			y <= mocegui.window[1].position.y + mocegui.window[1].size.y  then
 				mocegui.closeWindow()
 			end
 		end
 	end
 end
 
-function mocegui.mousepressed(x, y, button, istouch)
+function mocegui.mousepressed()
+	local x,y = rl.GetMouseX(),rl.GetMouseY()
 	if mocegui.window[1] then
-		if not (x >= mocegui.window[1].position[1]  and
-		x <= mocegui.window[1].position[1] + mocegui.window[1].size[1]  and
-		y >= mocegui.window[1].position[2]  and
-		y <= mocegui.window[1].position[2] + mocegui.window[1].size[2] ) then
-			for index, win in pairs(mocegui.window) do
-				if x >= win.position[1]  and
-				x <= win.position[1] + win.size[1]  and
-				y >= win.position[2]  and
-				y <= win.position[2] + win.size[2]  then
-					util.table.move(mocegui.window,index,1)
-					break
+		if rl.IsMouseButtonPressed(0) or rl.IsMouseButtonPressed(1) or rl.IsMouseButtonPressed(2) then
+			if not (x >= mocegui.window[1].position.x  and
+			x <= mocegui.window[1].position.x + mocegui.window[1].size.x  and
+			y >= mocegui.window[1].position.y  and
+			y <= mocegui.window[1].position.y + mocegui.window[1].size.y ) then
+				for index, win in pairs(mocegui.window) do
+					if x >= win.position.x  and
+					x <= win.position.x + win.size.x  and
+					y >= win.position.y  and
+					y <= win.position.y + win.size.y  then
+						util.table.move(mocegui.window,index,1)
+						break
+					end
 				end
 			end
 		end
-		if button == 1 then
-			if mocegui.window[1].title and
-			x >= mocegui.window[1].position[1]  and
-			x <= mocegui.window[1].position[1] + mocegui.window[1].size[1]  and
-			y >= mocegui.window[1].position[2]  and
-			y <= mocegui.window[1].position[2] + 20 then
-				mouse.draggin = mocegui.window[1].position
-				mouse.current = 2
-			end
+		if rl.IsMouseButtonPressed(0) then
 			for index, _button in ipairs(mocegui.window[1].button) do
-				if x >= mocegui.window[1].position[1] + _button.position[1]  and
-				x <= mocegui.window[1].position[1] + _button.position[1] + _button.size[1]  and
-				y >= mocegui.window[1].position[2] + _button.position[2]  and
-				y <= mocegui.window[1].position[2] + _button.position[2] + _button.size[2]  then
+				if x >= mocegui.window[1].position.x + _button.position.x  and
+				x <= mocegui.window[1].position.x + _button.position.x + _button.size.x  and
+				y >= mocegui.window[1].position.y + _button.position.y  and
+				y <= mocegui.window[1].position.y + _button.position.y + _button.size.y  then
 					_button.pressed = true
 				end
-			end
-		elseif button == 3 then
-			if x >= mocegui.window[1].position[1]  and
-			x <= mocegui.window[1].position[1] + mocegui.window[1].size[1]  and
-			y >= mocegui.window[1].position[2]  and
-			y <= mocegui.window[1].position[2] + mocegui.window[1].size[2]  then
-				mouse.draggin = mocegui.window[1].position
-				mouse.current = 2
 			end
 		end
 	end
 end
 
-function mocegui.update()
-
+function mocegui.mousedown()
+	local x,y = rl.GetMouseX(),rl.GetMouseY()
+	local delta = rl.GetMouseDelta()
+	if rl.IsMouseButtonDown(2) then
+		if x >= mocegui.window[1].position.x  and
+		x <= mocegui.window[1].position.x + mocegui.window[1].size.x  and
+		y >= mocegui.window[1].position.y  and
+		y <= mocegui.window[1].position.y + mocegui.window[1].size.y  then
+			mocegui.window[1].position.x = mocegui.window[1].position.x + delta.x
+			mocegui.window[1].position.y = mocegui.window[1].position.y + delta.y
+		end
+	elseif rl.IsMouseButtonDown(0) then
+		if mocegui.window[1].title and
+			x >= mocegui.window[1].position.x  and
+			x <= mocegui.window[1].position.x + mocegui.window[1].size.x  and
+			y >= mocegui.window[1].position.y  and
+			y <= mocegui.window[1].position.y + 20 then
+				mocegui.window[1].position.x = mocegui.window[1].position.x + delta.x
+				mocegui.window[1].position.y = mocegui.window[1].position.y + delta.y
+		end
+	end
 end
 
-function mocegui.draw()
+function mocegui.render()
+	mocegui.util.repeater(mocegui.pending)
+    if(rl.IsWindowResized()) then
+        rl.UnloadRenderTexture(options.rendertexture)
+        options.screen.x = rl.GetScreenWidth()
+        options.screen.y = rl.GetScreenHeight()
+        options.rendertexture = rl.LoadRenderTexture(options.screen.x, options.screen.y)
+        --world.redraw=true
+    end
+    mocegui.mousepressed()
+    mocegui.mousereleased()
+    mocegui.mousedown()
+    rl.BeginDrawing()
+    --if(world.redraw == true and options.freeze == false) then
+	rl.BeginTextureMode(options.rendertexture)
+	rl.ClearBackground(rl.BLACK)
+	--rl.BeginMode3D(options.camera)
+	--rl.EndMode3D()
 	util.array.selfclear(mocegui.window)
-	love.graphics.push()
+	--love.graphics.push()
 	for index = #mocegui.window, 1, -1 do
 		local v = mocegui.window[index]
 		if v.func then
 			v.func(v.args and unpack(v.args) or 0)
 		end
 		if v.title then
-			love.graphics.setColor(1,1,1,1)
-			love.graphics.rectangle('fill', v.position[1]-1, v.position[2]-1, v.size[1]+2, v.size[2]+2)
-			love.graphics.setColor(unpack(v.color))
-			love.graphics.rectangle('fill', v.position[1], v.position[2]+13, v.size[1], v.size[2]-13)
-			love.graphics.print(v.title,v.position[1],v.position[2],0,0.8,0.8)
+			rl.DrawRectangle(v.position.x-1, v.position.y-1, v.size.x+2, v.size.y+2, rl.WHITE)
+			rl.DrawRectangle(v.position.x, v.position.y+13, v.size.x, v.size.y-13,v.color)
+			if mocegui.font[1] then
+				rl.DrawTextEx(mocegui.font[1],v.title, {x=v.position.x,y=v.position.y}, 12, 0, v.color)
+			else
+				rl.DrawText(v.title, v.position.x,v.position.y, 12, v.color)
+			end
 		else
-			bRect(v.position[1], v.position[2], v.size[1], v.size[2], v.color)
+			bRect(v.position.x, v.position.y, v.size.x, v.size.y, v.color)
 		end
 		for key, button in ipairs(v.button) do
 			if button.func ~= mocegui.closeWindow then
-				bRect(v.position[1]+button.position[1], v.position[2]+button.position[2], button.size[1], button.size[2],(button.pressed and button.pcolor or button.color))
+				bRect(v.position.x+button.position.x, v.position.y+button.position.y, button.size.x, button.size.y,(button.pressed and button.pcolor or button.color))
 			else
-				love.graphics.setColor(unpack(button.pressed and button.pcolor or button.color))
-				love.graphics.rectangle('fill',v.position[1]+button.position[1], v.position[2]+button.position[2], button.size[1], button.size[2])
+				local c = button.pressed and button.pcolor or button.color
+				rl.DrawRectangle(v.position.x+button.position.x, v.position.y+button.position.y, button.size.x, button.size.y, c)
 			end
 		end
 		for key, text in ipairs(v.text) do
-			love.graphics.setColor(unpack(text.color))
-			love.graphics.print(text.text, v.position[1]+text.position[1], v.position[2]+text.position[2], 0,text.size[1], text.size[2])
+			if mocegui.font[1] then
+				rl.DrawTextEx(mocegui.font[1],text.text, {x=v.position.x+text.position.x, y=v.position.y+text.position.y}, 12, 0, text.color)
+			else
+				rl.DrawText(text.text, v.position.x+text.position.x, v.position.y+text.position.y, 12, text.color)
+			end
 		end
+			--world.redraw = false
+			--end
 	end
-	love.graphics.setColor(1,1,1,1)
-	love.graphics.pop()
+	rl.EndTextureMode();
+    rl.DrawTexturePro(
+        options.rendertexture.texture,
+        {
+            x=0,
+            y=0,
+            width=options.screen.x,
+            height=options.screen.y*-1
+        },
+        {x=0,y=0,width=options.screen.x,height=options.screen.y},
+        {x=0,y=0},
+        0,
+        rl.WHITE
+    );
+    rl.EndDrawing()
+end
+
+mocegui.close = function()
+	rl.CloseWindow()
 end
 
 mocegui.mouse = mouse
+
 return mocegui
